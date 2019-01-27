099fec9f096b28ed7b04ba5ba5b103fdb6670cf9
sanjayk98 <33611968+sanjayk98@users.noreply.github.com>
2019-01-27 13:53:08 -0500

    Add files via upload

--- NextWord/global.R
+++ NextWord/global.R
@@ -22,15 +22,19 @@ library(ggplot2)
 #destfile3<-"C:/Users/sanjayx/Desktop/coursera/swiftkey/Coursera-SwiftKey/final/en_US/model_trigram.RDS"
 #destfile4<-"C:/Users/sanjayx/Desktop/coursera/swiftkey/Coursera-SwiftKey/final/en_US/model_quadgram.RDS"
 
-destfile4<-"https://www.dropbox.com/s/x8iw5503silrrni/model_quadgram.RDS?dl=0"
-destfile3<-"https://www.dropbox.com/s/gjgw9ufyg1fk1ub/model_trigram.RDS?dl=0"
-destfile2<-"https://www.dropbox.com/s/26a427ox1s8c9sr/model_bigram.RDS?dl=0"
-destfile1<-"https://www.dropbox.com/s/czov141q5uby39l/model_unigram.RDS?dl=0"
+#destfile4<-"https://github.com/sanjayk98/predictNextWord/blob/master/model_quadgram.RDS"
+#destfile3<-"https://github.com/sanjayk98/predictNextWord/blob/master/model_trigram.RDS"
+#destfile2<-"https://github.com/sanjayk98/predictNextWord/blob/master/model_bigram.RDS"
+#destfile1<-"https://github.com/sanjayk98/predictNextWord/blob/master/model_unigram.RDS"
+destfile1<-"./data/model_unigram.RDS"
+destfile2<-"./data/model_bigram.RDS"
+destfile3<-"./data/model_trigram.RDS"
+destfile4<-"./data/model_quadgram.RDS"
 
-data1<-readRDS(url(destfile1))
-data2<-readRDS(url(destfile2))
-data3<-readRDS(url(destfile3))
-data4<-readRDS(url(destfile4))
+data1<-readRDS(destfile1)
+data2<-readRDS(destfile2)
+data3<-readRDS(destfile3)
+data4<-readRDS(destfile4)
 
 
 #nextword<-data.table(next_word=c("NA","NA","NA"),prob=c("NA","NA","NA"),Ngram=c("NA","NA","NA"))
